6745efde67dc21545aa710dabfcc153503fe37c1
Warun Panpaliya <warun1801@gmail.com>
2022-03-10 02:15:06 +0530

    fixed title

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ export default function App(props) {
   return (
     <>
       <Head>
-        <title>EasyCrypt</title>
+        <title>CryptEasy</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
 
