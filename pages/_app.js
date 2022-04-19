6c95ef5a580b8eddb509f756656df4734cb5ce54
Warun Panpaliya <51282650+warun1801@users.noreply.github.com>
2022-04-19 12:56:44 +0530

    added contrib samruddhi

--- pages/_app.js
+++ pages/_app.js
@@ -26,7 +26,7 @@ export default function App(props) {
             <Component {...pageProps} style={{height: "100vh", width: "100vw"}}/>
           </ScrollArea>
           <footer style={{ textAlign: "center", width: "100%", background: "#161616", minHeight: "20px", padding: "2px", marginTop: "auto"}}>
-            <Center><Text weight={500} size="sm">Made by Warun Panpaliya with </Text>&nbsp;<SiNextdotjs size="20px"/>&nbsp;<Text weight={500} size="sm">NextJs and </Text>&nbsp;<SiFastapi size="20px" />&nbsp;<Text weight={500} size="sm">FastApi.</Text></Center>
+            <Center><Text weight={500} size="sm">Made by Warun Panpaliya and Samruddhi Pande with </Text>&nbsp;<SiNextdotjs size="20px"/>&nbsp;<Text weight={500} size="sm">NextJs and </Text>&nbsp;<SiFastapi size="20px" />&nbsp;<Text weight={500} size="sm">FastApi.</Text></Center>
             <Center><FaRegCopyright size="10px" style={{color: "#707172"}}/>&nbsp;<Text size="xs" weight={500} color="#707172">Copyright 2022. All rights reserved by Warun Panpaliya.</Text></Center>
           </footer> 
         </ScrollArea>
